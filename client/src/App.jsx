ddf53c1a1564ac3d2be14615504db582c41e6c3b
Olya74 <olyayoga74@gmail.com>
2025-04-28 15:29:07 +0200

    client structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Title from './components/title/Title'
+import List from './components/poposList/PlacesLists'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
